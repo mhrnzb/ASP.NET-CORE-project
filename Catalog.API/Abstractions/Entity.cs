5b5ec75117a5a7e64fa1374d3ae6d74f4762fbd7
mehrnaz <mehrnazbg1381@gmail.com>
2023-09-09 19:04:20 +0330

    interfaces are updated

--- Catalog.API/Abstractions/Entity.cs
+++ Catalog.API/Abstractions/Entity.cs
@@ -1,7 +1,7 @@
-
+using Catalog.API.Application.Contracts;
 namespace Catalog.API.Abstractions
 {
-    public class Entity<TId>
+    public class Entity<TId> : IEntity<TId>
     {
         public TId? Id { get; set; }
     }
--- Catalog.API/Application/Contracts/IRepository.cs
+++ Catalog.API/Application/Contracts/IRepository.cs
@@ -1,9 +1,9 @@
-using Catalog.API.Abstractions;
+
 namespace Catalog.API.Application.Contracts
 { 
     //? product/customer
     public interface IRepository<TEntity , TId>
-    where TEntity : Entity<TId>
+    where TEntity : IEntity<TId>
     { 
 
         Task AddAsync(TEntity entity);
