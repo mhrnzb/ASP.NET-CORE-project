915337217691ee571d37c7b6d8cd7f77c0354f0b
mehrnaz <mehrnazbg1381@gmail.com>
2023-09-09 17:00:06 +0330

    product poco class added

---
+++ Catalog.API/Models/Product.cs
@@ -0,0 +1,16 @@
+
+namespace Catalog.API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public string? Description { get; set; }
+
+
+    }
+}
