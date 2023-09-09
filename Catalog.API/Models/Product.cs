6c94e3f6b53b0bab7ec72060bf10f73d7f691aa0
mehrnaz <mehrnazbg1381@gmail.com>
2023-09-09 17:37:04 +0330

    Customer.cs added

--- Catalog.API/Models/Product.cs
+++ Catalog.API/Models/Product.cs
@@ -4,7 +4,9 @@ namespace Catalog.API.Models
     //? Anemic model 
     public class Product
     {
+        //?Guid , Identity , Custom value(string )
         public int Id { get; set; }
+
 
         public string Name { get; set; }
 
