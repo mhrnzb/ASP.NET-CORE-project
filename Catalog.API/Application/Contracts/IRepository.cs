d64c756d71c9bc30a5a66a328e7e9f3f5b51bd15
mehrnaz <mehrnazbg1381@gmail.com>
2023-09-09 18:45:02 +0330

    IRepository updated

--- Catalog.API/Application/Contracts/IRepository.cs
+++ Catalog.API/Application/Contracts/IRepository.cs
@@ -1,9 +1,11 @@
-
+using Catalog.API.Abstractions;
 namespace Catalog.API.Application.Contracts
 { 
     //? product/customer
-    public interface IRepository<TEntity>
+    public interface IRepository<TEntity , TId>
+    where TEntity : Entity<TId>
     { 
+
         Task AddAsync(TEntity entity);
 
         Task <IEnumerable<TEntity>> GetEntitiesAsync();
