4fe54b5e98fe635fa7f2a4c8721a5741239cd887
mehrnaz <mehrnazbg1381@gmail.com>
2023-09-09 19:22:56 +0330

    repository.cs added

--- Catalog.API/Infrastructure/Repository.cs
+++ Catalog.API/Infrastructure/Repository.cs
@@ -1,12 +1,25 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Catalog.API.Application.Contracts;
+using Catalog.API.Abstractions;
 
 namespace Catalog.API.Infrastructure
 {
-    public class Repository
-    {
-        
+    public class Repository<TEntity , TId> : IRepository<TEntity , TId>
+    where TEntity : IEntity<TId>
+   {
+
+    public Task AddAsync(TEntity entity){
+        throw new NotImplementedException();
     }
+
+
+    public Task<IEnumerable<TEntity>> GetEntitiesAsync(){
+        throw new NotImplementedException();
+    }
+
+
+
+
+
+
+   }
 }
