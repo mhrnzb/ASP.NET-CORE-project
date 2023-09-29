82c734138cca91c2f50377fc22563750e149a71f
mehrnaz <mehrnazbg1381@gmail.com>
2023-09-29 12:48:32 +0330

    some statements commneted in inital.sql

--- Catalog.API/Infrastructure/SQL/Initial.sql
+++ Catalog.API/Infrastructure/SQL/Initial.sql
@@ -1,12 +1,12 @@
-﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
-BEGIN
-    CREATE TABLE [__EFMigrationsHistory] (
-        [MigrationId] nvarchar(150) NOT NULL,
-        [ProductVersion] nvarchar(32) NOT NULL,
-        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
-    );
-END;
-GO
+﻿-- IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+-- BEGIN
+--     CREATE TABLE [__EFMigrationsHistory] (
+--         [MigrationId] nvarchar(150) NOT NULL,
+--         [ProductVersion] nvarchar(32) NOT NULL,
+--         CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+--     );
+-- END;
+-- GO
 
 BEGIN TRANSACTION;
 GO
@@ -23,9 +23,9 @@ CREATE TABLE [Products] (
 );
 GO
 
-INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20230904165605_Initial', N'7.0.10');
-GO
+-- INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+-- VALUES (N'20230904165605_Initial', N'7.0.10');
+-- GO
 
 COMMIT;
 GO
