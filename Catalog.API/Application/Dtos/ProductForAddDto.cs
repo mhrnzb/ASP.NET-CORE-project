3f26dded84a114477d1263b946a562c69d2dd74b
mehrnaz <mehrnazbg1381@gmail.com>
2023-09-29 13:47:29 +0330

    ProductForAddDto.cs implemented

--- Catalog.API/Application/Dtos/ProductForAddDto.cs
+++ Catalog.API/Application/Dtos/ProductForAddDto.cs
@@ -2,6 +2,8 @@ namespace Catalog.API.Application.Dtos
 {
     public class ProductForAddDto
     {
-        
+        public string Name {get; set;}
+        public int Price {get; set;}
+        public string Description{get; set;}
     }
 }
