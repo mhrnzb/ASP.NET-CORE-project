4eb0daa7ae5893fffc67e45826486dacaea252f5
mehrnaz <mehrnazbg1381@gmail.com>
2023-09-29 12:43:52 +0330

    inital.sql added

---
+++ Catalog.API/Infrastructure/SQL/Initial.sql
@@ -0,0 +1,32 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE SEQUENCE [ProductSeq] START WITH 1 INCREMENT BY 10 NO MINVALUE NO MAXVALUE NO CYCLE;
+GO
+
+CREATE TABLE [Products] (
+    [Id] int NOT NULL,
+    [Name] nvarchar(100) NOT NULL,
+    [Price] int NOT NULL,
+    [Description] nvarchar(20) NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY ([Id])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20230904165605_Initial', N'7.0.10');
+GO
+
+COMMIT;
+GO
+
