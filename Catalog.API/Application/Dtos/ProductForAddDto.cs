765de98003d6120569af00b509bc6802c12d43ae
mehrnaz <mehrnazbg1381@gmail.com>
2023-09-29 13:49:59 +0330

    comment added

--- Catalog.API/Application/Dtos/ProductForAddDto.cs
+++ Catalog.API/Application/Dtos/ProductForAddDto.cs
@@ -1,9 +1,10 @@
-namespace Catalog.API.Application.Dtos
-{
+namespace Catalog.API.Application.Dtos;
+//? Data Transfer Object (DTO)
+//? Immutable
+
     public class ProductForAddDto
     {
         public string Name {get; set;}
         public int Price {get; set;}
         public string Description{get; set;}
     }
-}
