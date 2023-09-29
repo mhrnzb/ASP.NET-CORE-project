425f795c023b05536189336ca0f95d3d0410d868
mehrnaz <mehrnazbg1381@gmail.com>
2023-09-29 13:04:59 +0330

    repository implemented

--- Catalog.API/Infrastructure/Repository.cs
+++ Catalog.API/Infrastructure/Repository.cs
@@ -1,15 +1,30 @@
 using Catalog.API.Application.Contracts;
 using Catalog.API.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Catalog.API.Infrastructure;
 
-//? Fake
+
 public abstract class Repository<TEntity, TId> : IRepository<TEntity, TId>
 where TEntity : Entity<TId>
 {
-    public Task AddAsync(TEntity entity)
+    private DbContext _context;
+
+    private DbSet<TEntity> _set;
+
+
+    protected Repository(DbContext context)
     {
-        throw new NotImplementedException();
+        _context = context;
+        _set = _context.Set<TEntity>();
+
+    }
+
+
+    public  async Task AddAsync(TEntity entity)
+    {
+       await _set.AddAsync(entity);
     }
 
     public Task<IEnumerable<TEntity>> GetEntitiesAsync()
