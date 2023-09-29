ea84ed2209e665c0cc8b152ec927fe400d9ad311
mehrnaz <mehrnazbg1381@gmail.com>
2023-09-29 13:22:51 +0330

    ProductRepository.cs implemneted

--- Catalog.API/Infrastructure/Repositories/ProductRepository.cs
+++ Catalog.API/Infrastructure/Repositories/ProductRepository.cs
@@ -1,9 +1,17 @@
 using Catalog.API.Application.Contracts;
 using Catalog.API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Catalog.API.Infrastructure.Repositories;
 
-public class ProductRepository : Repository<Product, int> , IProductRepository
+public sealed class ProductRepository : Repository<Product, int>, IProductRepository
 {
-    
+    public ProductRepository(DbContext context) : base(context)
+    {
+        public ProductRepository(DbContext context) : base(context)
+
+        {
+
+        }
+    }
 }
