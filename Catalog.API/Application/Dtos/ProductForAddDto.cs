a4299099642a4d4bc8a0af526b2ee706af00507e
mehrnaz <mehrnazbg1381@gmail.com>
2023-09-29 13:58:38 +0330

    rewrite ProductForAddDto with record method

--- Catalog.API/Application/Dtos/ProductForAddDto.cs
+++ Catalog.API/Application/Dtos/ProductForAddDto.cs
@@ -2,21 +2,24 @@ namespace Catalog.API.Application.Dtos;
 //? Data Transfer Object (DTO)
 //? Immutable
 
-    public class ProductForAddDto
-    {
-        public string Name {get; init;}
-        public int Price {get; init;}
-        public string Description{get; init;}
+    // public  sealed class ProductForAddDto
+    // {
+    //     public string Name {get; init;}
+    //     public int Price {get; init;}
+    //     public string Description{get; init;}
 
-        public ProductForAddDto(string name , int price , string description){
-            Name = name ;
-            Price = price;
-            Description = description;
-        }
+    //     public ProductForAddDto(string name , int price , string description){
+    //         Name = name ;
+    //         Price = price;
+    //         Description = description;
+    //     }
 
-
-
-
+    // }
 
 
-    }
+public record ProductForAddDto(
+    string Name,
+    int Price,
+    string Description
+
+);
