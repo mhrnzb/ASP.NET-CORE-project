eb90b93faaa86d7539168fca5ffcef280c0a31b0
mehrnaz <mehrnazbg1381@gmail.com>
2023-10-05 19:29:31 +0330

    productMapper.cs added

---
+++ Catalog.API/Application/Mappers/ProductMappers.cs
@@ -0,0 +1,15 @@
+
+using AutoMapper;
+using Catalog.API.Application.Dtos;
+using Catalog.API.Models;
+
+namespace Catalog.API.Application.Mappers;
+
+public class ProductMappers : Profile
+{
+    public ProductMappers()
+    {
+        CreateMap<ProductForUpdateDto, Product>();
+        CreateMap<Product, ProductDto>();
+    }
+}
